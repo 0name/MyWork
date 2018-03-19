2bcfb763b246a227387a9ae91b933c4c3f7084b2
Dan11l <noname.spb@mail.ru>
2018-03-19 22:10:57 +0300

    first commit

---
+++ FirstProgramm.java
@@ -0,0 +1,43 @@
+public class FirstProgramm {
+    // public - модификатрор доступа
+    /*
+    *
+    *
+    *
+    */
+    public static void main(String [] args){
+        // Типы данных
+        // Примитивные типы
+        /*
+        int
+        byte
+        char
+        long
+        double
+        float
+        boolean
+        */
+        int intVarName; // объявление переменной
+        boolean boolVarName;
+        int intVarName1, intVarName2,intVarName3;
+
+        intVarName = 0; // присвоение значений
+
+        int someVar = 9;
+
+        someVar = 2;
+        // Ссылочные типы
+        // String
+
+        String strVar = "String var value";
+
+        // опероаторы
+        // + = * / %
+        // Integer_
+        System.out.println( strVar);
+
+
+        // git config --global user.name "Имя"
+        // git config --global user.email email
+    }
+}
