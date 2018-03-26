0fc873c331d50e668b945c58b0df8b1603ac887e
Dan11l <noname.spb@mail.ru>
2018-03-26 11:59:37 +0300

    Обновлен файл MassivEx5.java

--- MassivEx5.java
+++ MassivEx5.java
@@ -6,11 +6,18 @@ public class MassivEx5 {
 //    Определить и вывести на экран сообщение о том, является ли массив строго возрастающей последовательностью.
     public static void main(String[] args){
         int arr5 [] = new int[4];
+        boolean status = false;
         Random random = new Random();
 
         for (int i = 0; i < arr5.length ; i++ ) {
             arr5[i] = random.nextInt(100) + 10;
+//            if (arr5[i] < arr5 [i + 1])
+//                status = 1;
         }
+        int arr52 [] = arr5.clone();
+        Arrays.sort(arr52);
+
         System.out.println(Arrays.toString(arr5));
+        System.out.println( Arrays.equals(arr5, arr52) ? "Массив возрастающий" : "Массив НЕ ВОЗРАСТАЮЩИЙ ");
     }
 }
