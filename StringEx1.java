df3ea623a05e4a8cb126a402515516cd283139a4
Dan11l <noname.spb@mail.ru>
2018-03-27 11:37:39 +0300

    Изменен файл StringEx1.java

--- StringEx1.java
+++ StringEx1.java
@@ -4,13 +4,27 @@ public class StringEx1 {
 //    1) Найти в строке указанную подстроку и заменить ее на новую. Строку, ее подстроку для замены и новую подстроку вводит пользователь.
     public static void main(String [] args){
         Scanner in = new Scanner(System.in);
-        System.out.println("Enter a string ");
-        int input = in.nextInt();
+        String input = new String(" Люблю грозу в начале мая, когда весенний первый гром ... ");
+    //    System.out.println("Enter a string ");
+    //    String input = in.next();
 
-        System.out.println("Entered string: " + input);
+        input =  input.trim();
+        System.out.println("Введите строку: " + input);
 
-        System.out.println("Enter a substring ");
-
-        System.out.println("New string: " + input);
+        System.out.println("Введите подстроку для поиска ");
+        String inputSub = in.next();
+            while(!(input.contains(inputSub))) {
+                System.out.println("Введенная подстрока не найдена! \n Повторите ввод или наберите 'выход' ");
+                inputSub = in.next();
+                if(inputSub.contains("выход")) {
+                    break;
+                }
+            }
+        if (input.contains(inputSub)) {
+            System.out.println("Введенная подстрока найдена \n Введите новую строку ");
+            String newInputSub = in.next();
+            String newStr = input.replace(inputSub, newInputSub);
+            System.out.println("Новая строка: " + newStr);
+        }
     }
 }
