062715fa0bc64d0d2fdc93ced61881082108cd7b
Dan11l <noname.spb@mail.ru>
2018-03-27 06:51:43 +0300

    Homework 26.03

--- StringEx1.java
+++ StringEx1.java
@@ -1,6 +1,16 @@
+import java.util.Scanner;
+
 public class StringEx1 {
 //    1) Найти в строке указанную подстроку и заменить ее на новую. Строку, ее подстроку для замены и новую подстроку вводит пользователь.
     public static void main(String [] args){
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter a string ");
+        int input = in.nextInt();
 
+        System.out.println("Entered string: " + input);
+
+        System.out.println("Enter a substring ");
+
+        System.out.println("New string: " + input);
     }
 }
