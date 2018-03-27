507e309855c9b5df423d323b84258042cb96c9d4
Dan11l <noname.spb@mail.ru>
2018-03-27 06:41:02 +0300

    Homework 26.03

---
+++ MassivEx6.java
@@ -0,0 +1,4 @@
+package PACKAGE_NAME;
+
+public class MassivEx6 {
+}
---
+++ StringEx2.java
@@ -0,0 +1,6 @@
+public class StringEx2 {
+//    2) Требуется удалить из нее повторяющиеся символы и все пробелы. Например, если было введено "abc cde def", то должно быть выведено "abcdef".
+    public static void main(String [] args){
+
+    }
+}
