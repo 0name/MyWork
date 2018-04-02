e77a3c8a91a32cfc15c725e44c15246ff61d734c
Dan11l <noname.spb@mail.ru>
2018-04-02 17:34:42 +0300

    Добавил пакет School

---
+++ School/Personal/Teachers/Teacher.java
@@ -0,0 +1,27 @@
+package School.Personal.Teachers;
+
+import School.People;
+import School.Pupils.Pupil;
+
+public class Teacher extends People {
+    String predmet;
+    int stage;// стаж
+
+public Teacher(String name, int age, String status, String predmet, int stage) {
+    super(name, age, status);
+    this.predmet =  predmet;
+    this.stage = stage;
+    }
+
+    public void say(){
+        System.out.println("I'm a " + predmet + " teacher.\nI've been teaching for " + stage + " years." );
+    }
+
+    public void putGoodAstimate(){ // поставить хорошую оценку
+        System.out.println("Today I put a good raiting " );
+    }
+
+    public void putBadAstimate(){ // поставить плохую оценку
+
+    }
+}
--- School/Pupils/Pupil.java
+++ School/Pupils/Pupil.java
@@ -1,7 +1,18 @@
 package School.Pupils;
 
-public class Pupil {
+import School.People;
+
+public class Pupil extends People {
     int numOfClass;
+
+    public Pupil(String name, int age, String status, int numOfClass){
+        super(name, age, status);
+        this.numOfClass = numOfClass;
+    }
+
+    public void say (){
+        System.out.println("I'm in the " + numOfClass + "th grade");
+}
 
 //    могут посетить урок
 //    могут пропустить урок
